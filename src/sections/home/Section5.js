9ec019fb395cb824c7634efee3458a27d5eeafd1
rjg007 <rahul@codewave.com>
2022-01-25 15:18:19 +0530

    Content Changes

--- src/sections/home/Section5.js
+++ src/sections/home/Section5.js
@@ -226,150 +226,78 @@ const Section5 = ({ className, ...rest }) => {
   };
 
   return (
-    <>
-     <div className={className} {...rest}>
+//     <>
+//      <div className={className} {...rest}>
        
-       <div class="content-section  pt-28 pb-8 bg-default-4" id="hethkrndmr">
-       <div class="container">
-         <div class="row align-items-center pt-20">
-           <div class="col-lg-6" id="ayaya-paap" data-aos="flip-right" data-aos-duration="1000" data-aos-once="true">
-             {/* <div class="content-img sxrty">
-             <img src={ homesection3 } alt="" />
-             </div> */}
+//        <div class="content-section  pt-28 pb-8 bg-default-4" id="hethkrndmr">
+//        <div class="container">
+//          <div class="row align-items-center pt-20">
+//            <div class="col-lg-6" id="ayaya-paap" data-aos="flip-right" data-aos-duration="1000" data-aos-once="true">
+            
  
          
  
-             <div className="content-img " id="twsdf-fgdrt">
-             <div className="l6-content-image-group-2-new">
-               <div className="img-1">
-                 <img
-                   className=""
-                   src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621250871/Group_101_pqsme8.png"}
-                   alt=""
-                   data-aos="fade-down"
-                   data-aos-delay={800}
-                 />
-               </div>
-               <div className="img-2">
-                 <img
-                   className="ltrdtyu"
-                   src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1625509007/Group_52_Copy_trvpxs-min_k2yrwn.png"}
-                   alt=""
-                   data-aos="fade-right"
-                   data-aos-delay={500}
-                 />
-               </div>
-             </div>
-           </div>
+//              <div className="content-img " id="twsdf-fgdrt">
+//              <div className="l6-content-image-group-2-new">
+//                <div className="img-1">
+//                  <img
+//                    className=""
+//                    src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621250871/Group_101_pqsme8.png"}
+//                    alt=""
+//                    data-aos="fade-down"
+//                    data-aos-delay={800}
+//                  />
+//                </div>
+//                <div className="img-2">
+//                  <img
+//                    className="ltrdtyu"
+//                    src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1625509007/Group_52_Copy_trvpxs-min_k2yrwn.png"}
+//                    alt=""
+//                    data-aos="fade-right"
+//                    data-aos-delay={500}
+//                  />
+//                </div>
+//              </div>
+//            </div>
  
  
-           </div>
-           <div class="col-10 col-lg-6 imgs-imt">
-             <div class="section-title content-text mb-13" data-aos="fade-left" data-aos-duration="500" data-aos-once="true">
-               <h2 class="title gr-text-3 mb-10 engag mt-0" id="nin-newzlnd">Mentorship</h2>
-               <p class="gr-text-8 clscls" id="armdst-weundstrd">We understand the role of a teacher in preparing a student both mentally and emotionally on the education portal. Therefore, our teachers are dedicated to giving individual attention to each child.</p>
-               {/* <a href="#" class="btn btn-red mx-auto gtstrdr">Our Mentors</a> */}
-             </div>
-             <div class="content-widget">
-               <div class="row">
+//            </div>
+//            <div class="col-10 col-lg-6 imgs-imt">
+//              <div class="section-title content-text mb-13" data-aos="fade-left" data-aos-duration="500" data-aos-once="true">
+//                <h2 class="title gr-text-3 mb-10 engag mt-0" id="nin-newzlnd">Mentorship</h2>
+//                <p class="gr-text-8 clscls" id="armdst-weundstrd">We understand the role of a teacher in preparing a student both mentally and emotionally on the education portal. Therefore, our teachers are dedicated to giving individual attention to each child.</p>
+//              </div>
+//              <div class="content-widget">
+//                <div class="row">
                  
-               </div>
-             </div>
-           </div>
-         </div>
+//                </div>
+//              </div>
+//            </div>
+//          </div>
  
-         {/* <div class="row align-items-center justify-content-center">
-             
-             <div class="col-10 col-lg-6 col-xl-6 pr-xl-13 order-lg-1 formob">
-               <div class="section-title content-text mb-13" data-aos="fade-left" data-aos-duration="500" data-aos-once="true">
-                 <h2 class="title gr-text-3 mb-6 engag">Doubt solving</h2>
-                 <p class="gr-text-8 clscls">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                  <a href="#" class="btn btn-red mx-auto gtstrdr">Learn more</a>
-               </div>
-               <div class="content-widget">
-                 <div class="row mb-n9">
-                  
-                 </div>
-               </div>
-             </div>
- 
-             <div class="col-lg-6 order-lg-2 extclt" data-aos="flip-right" data-aos-duration="2000" data-aos-once="true">
-               <div class="content-img text-right">
-               
-               <div className="l6-content-image-group-2-new">
-               <div className="img-1">
-                 <img
-                   className=""
-                   src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621250871/Group_101_pqsme8.png"}
-                   alt=""
-                   data-aos="fade-down"
-                   data-aos-delay={800}
-                 />
-               </div>
-               <div className="img-2">
-                 <img
-                   className="ltrdtyu"
-                   src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621255964/My_doubts_2x_bv1kp4.png"}
-                   alt=""
-                   data-aos="fade-right"
-                   data-aos-delay={500}
-                 />
-               </div>
-             </div>
-               
-               </div>
-             </div>
- 
- 
-           </div> */}
- 
- 
- 
-   
-   
-   {/* <div className="l6-content-image-group-4-new">
-               <div className="img-1">
-                 <img
-                   className=""
-                   src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621256211/Rectangle_jns94x.png"}
-                   alt=""
-                   data-aos="fade-down"
-                   data-aos-delay={800}
-                 />
-               </div>
-               <div className="img-2">
-                 <img
-                   className=""
-                   src={"https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621255964/My_doubts_2x_bv1kp4.png"}
-                   alt=""
-                   data-aos="fade-right"
-                   data-aos-delay={500}
-                 />
-               </div>
-             </div> */}
+         
 
-<div class="row align-items-center pt-20 justify-content-center" id="yvhdstagl">
+// <div class="row align-items-center pt-20 justify-content-center" id="yvhdstagl">
 
-<div class="col-10 col-lg-6 col-xl-6 pr-xl-13 order-lg-1 formob">
-  <div class="section-title content-text mb-13" data-aos="fade-left" data-aos-duration="500" data-aos-once="true">
-    <h2 class="title gr-text-3 mb-6 engag mt-0">Assessments & Analytics</h2>
-    <p class="gr-text-8 clscls" id="dvr-elr-wn">Solve class 8 to class 10 exam questions created by subject matter experts with decades of experience to outperform your competitors. Get access to thousands of topic-wise questions, ranging between all levels of difficulty. </p>
-      {/* <a href="#" class="btn btn-red mx-auto gtstrdr">Practice tests</a> */}
-  </div>
-  <div class="content-widget">
-    <div class="row mb-n9">
+// <div class="col-10 col-lg-6 col-xl-6 pr-xl-13 order-lg-1 formob">
+//   <div class="section-title content-text mb-13" data-aos="fade-left" data-aos-duration="500" data-aos-once="true">
+//     <h2 class="title gr-text-3 mb-6 engag mt-0">Assessments & Analytics</h2>
+//     <p class="gr-text-8 clscls" id="dvr-elr-wn">Solve class 8 to class 10 exam questions created by subject matter experts with decades of experience to outperform your competitors. Get access to thousands of topic-wise questions, ranging between all levels of difficulty. </p>
+//   </div>
+//   <div class="content-widget">
+//     <div class="row mb-n9">
      
-    </div>
-  </div>
-</div>
+//     </div>
+//   </div>
+// </div>
 
-<div class="col-lg-6 order-lg-2 " data-aos="flip-right" data-aos-duration="2000" data-aos-once="true">
-  <div class="content-img text-right">
-  <img class="dart-elil" src={ "https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621277812/Dashboard_Overview_2x_pj1fff.png" } alt="" />
-  </div>
-</div>
+// <div class="col-lg-6 order-lg-2 " data-aos="flip-right" data-aos-duration="2000" data-aos-once="true">
+//   <div class="content-img text-right">
+//   <img class="dart-elil" src={ "https://res.cloudinary.com/ddo1ag5nz/image/upload/v1621277812/Dashboard_Overview_2x_pj1fff.png" } alt="" />
+//   </div>
+// </div>
 
-</div>
+// </div>
    
    
   
@@ -377,12 +305,96 @@ const Section5 = ({ className, ...rest }) => {
 
  
  
-       </div>
-     </div>
+//        </div>
+//      </div>
          
          
-       </div>
-    </>
+//        </div>
+//     </>
+<div 
+      className="container py-5 my-5"
+      style={{
+        backgroundColor: "#fff"
+      }}
+    >
+      <div className="row align-items-center">
+        <h2 
+          style={{
+            fontWeight: 100
+          }}
+          className="section-2-heading-phone mx-auto"
+        >
+          Continuing Education
+        </h2>
+        <div 
+          className="col-md-6" 
+          data-aos="flip-right" 
+          data-aos-duration="1000" 
+          data-aos-once="true"
+        >
+          <img 
+            src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1643102569/home-page/Continuing-Education_1_er7hfb.png" 
+            alt="" 
+            className="img-fluid"
+          />
+        </div>
+        <div className="col-md-6">
+          <h2 
+            style={{
+              fontWeight: 100
+            }}
+            className="section-2-heading-desktop"
+            data-aos="fade-left" 
+            data-aos-duration="500" 
+            data-aos-once="true"
+          >
+            Continuing Education
+          </h2>
+          <p>
+            Our Continuing Education program primarily focuses on imparting knowledge and career-oriented skills to learners from any part of the world, with no boundaries around the learners with regard to their age, restricted physical classrooms, etc.
+          </p>
+        </div>
+      </div>
+
+      <div className="row align-items-center">
+        <h2 
+          style={{
+            fontWeight: 100
+          }}
+          className="section-2-heading-phone mx-auto"
+        >
+          An Alarming fact in the Indian Education System
+        </h2>
+        <div 
+          className="col-md-6 order-md-last" 
+          data-aos="flip-right" 
+          data-aos-duration="1000" 
+          data-aos-once="true"
+        >
+          <img 
+            src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1643103341/home-page/An-Alarming-fact-in-the-Indian-Education-System-PNG_1_huawpv.png" 
+            alt="" 
+            className="img-fluid"
+          />
+        </div>
+        <div className="col-md-6">
+          <h2 
+            style={{
+              fontWeight: 100
+            }}
+            className="section-2-heading-desktop"
+            data-aos="fade-left" 
+            data-aos-duration="500" 
+            data-aos-once="true"
+          >
+            An Alarming fact in the Indian Education System
+          </h2>
+          <p>
+            Around 35,354,000 students are dropping out of the education system and the disturbing factor is that it keeps on increasing each day. 
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
 
