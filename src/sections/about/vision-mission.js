b67837eb300f1fadcd548bf4e5a4e99198e5f31a
rjg007 <rahul@codewave.com>
2022-01-25 16:39:56 +0530

    About us page changes

--- src/sections/about/vision-mission.js
+++ src/sections/about/vision-mission.js
@@ -24,14 +24,14 @@ const VisionMission = ({ className, ...rest }) => {
             <div class="row">
               <div class="col-md-6 col-lg-11 col-xl-11" data-aos="fade-left" data-aos-duration="500" data-aos-once="true">
                 <div class="single-widget mb-9">
-                  <h3 class="w-title gr-text-7 abt-ourmis">Why us?</h3>
-                  <p class="gr-text-9 abt-trty">We wish to deepen our roots and provide education in almost every stream, ranging from science and commerce to arts and law. Whether it's a tedious maths problem or a tricky science experiment, Rankpedia explains it in a creative and electrifying way. We have brilliant and exceptional educators with extraordinary teaching strategies, which form the base of our online education.  </p>
+                  <h3 class="w-title gr-text-7 abt-ourmis">Our Vision</h3>
+                  <p class="gr-text-9 abt-trty">Our objective is to make a diverse array of online education opportunities accessible to students around the globe. Every child has the right to education, and we are attempting to reach out to those children who are unable to receive an education due to a variety of factors, including social and economic restraints. We aim to bring back those children who have dropped out of school for a variety of reasons into the education system. We want to increase kids' enthusiasm for learning by integrating innovative techniques. </p>
                 </div>
               </div>
               <div class="col-md-6 col-lg-11 col-xl-11" data-aos="fade-left" data-aos-duration="1100" data-aos-once="true">
                 <div class="single-widget mb-9">
-                  <h3 class="w-title gr-text-7 abt-ourmis">Our belief is in giving your child relief!</h3>
-                  <p class="gr-text-9 abt-trty">It started with a simple thought of 'Why can't world-class education be affordable?'. This thought embedded a seed in our mind and gave us a vision of providing education to children at an affordable price. We have walked on a path to deliver world-class education by integrating pedagogy and the latest technological advancements to offer the best opportunities to students.  </p>
+                  <h3 class="w-title gr-text-7 abt-ourmis">Our Mission</h3>
+                  <p class="gr-text-9 abt-trty">We aim to provide learners with a rich and diverse learning environment by incorporating international curricula with technologically advanced pedagogical practices. We recognize the significance of holistic education, thus, we have adopted effective teaching methodologies that promote outcome-based experiential learning. We provide an equal education platform and high-quality learning content to learners, assisting them in developing solid conceptual foundations and improving their academic and career prospects. We've also embarked on a journey to bridge the digital divide that exists here, by providing education to those children who belong to remote areas, for whom receiving a quality education is a far-fetched dream.</p>
                 </div>
               </div>
               <div class="col-md-6 col-lg-11 col-xl-11" data-aos="fade-left" data-aos-duration="1100" data-aos-once="true">
