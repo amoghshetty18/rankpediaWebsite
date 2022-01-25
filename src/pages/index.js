72a07934f7a8c70db2d0860a1a1de7b54a5ee94a
rjg007 <rahul@codewave.com>
2022-01-25 11:49:05 +0530

    section 1 content changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,15 +35,10 @@ const IndexPage = () => {
           headerClassName: "site-header--menu-right",
           headerButton: (
             <>
-            <a href="/login">  <button className="btn btn btn-blue-3 header-btn1 head-login">
-                Login
-              </button> </a>
-
-              {/* <div class="header-btn  ml-auto ml-lg-5 mr-6 mr-lg-0 d-none d-xs-block" id="login-tpr">
-            <a class="btn bggr gr-text-9 d-none d-xs-inline-flex " href="#">
-              Login
-            </a>
-          </div> */}
+              <a href="/login">  <button className="btn btn btn-blue-3 header-btn1 head-login">
+                  Login
+                </button> 
+              </a>
             </>
           ),
           footerStyle: "style3",
@@ -58,12 +53,6 @@ const IndexPage = () => {
         <Section6 />
         <Section7 />
         <Section8 />
-        {/* <Hero className="position-relative z-index-1" />
-        <Company className="bg-default-1 pt-13 pt-md-17 pt-lg-24 pb-13 pb-md-14 pb-lg-23" />
-        <Categories className="bg-default-1 pt-lg-13 pb-md-7 pb-lg-11 position-relative" />
-        <Jobs className="pt-14 pt-md-18 pt-lg-30 bg-default-2 pb-9 pb-md-13 pb-lg-29" />
-        <Content1 className="pt-21 pt-md-24 pt-lg-32 pb-15 pb-md-19 pb-lg-30" />
-        <Blog className="bg-default-1 pt-14 pt-md-18 pt-lg-27 pb-13 pb-md-17 pb-lg-26" /> */}
       </PageWrapper>
     </>
   );
--- src/sections/home/Section1.js
+++ src/sections/home/Section1.js
@@ -24,51 +24,12 @@ const Section1 =  ({ className, ...rest }) => {
     <>
       <div className={className} {...rest}>
         <div class="hero-area"  id="roller-bnr">
-      
-          {/* <div class="container">
-            <div class="row justify-content-center align-items-center">
-              <div class="col-lg-8 col-md-9 order-lg-1 mt-9 mt-lg-0 tt-sct" data-aos="fade-right" data-aos-duration="500" data-aos-once="true">
-                <div class="hero-content dark-mode-texts">
-                  <h1 class="gr-text-2 mb-9 learnon">Smarter Learning for a Smarter Generation</h1>
-                  <p class="gr-text-8 pr-5 pl-0 pr-xl-11 mb-9 learnpar">When great teachers meet great technology, learning becomes easy. Enjoy trouble-free learning, and boost your preparation with RankPedia to unlock new possibilities.  </p>
-                  <div class="hero-btn">
-                    <a href="#" class="btn-link with-icon text-green1 gr-text-7 font-weight-bold">Get Started <i class="icon icon-tail-right font-weight-bold"></i></a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4 col-md-9 order-lg-2">
-                <div class="hero-img" data-aos="fade-left" data-aos-duration="500" data-aos-once="true">
-                  <div class="hero-video-thumb position-relative">
-                  <img src={ "https://res.cloudinary.com/ddo1ag5nz/image/upload/v1625508835/banner-0ff164b5601283a714f182a6c289998b_1_-min_fcvjts.png" } class="clrs-rtd rounded-8" alt="" />
-                    <div className='blob-container'>
-                      <img id='main-background-img' src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1626160479/home-page/banner_mggspd.png" alt="" />
-                      <img className="science" src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1627981630/logo_placeholder_sm_ra9ybv.png" alt="" />
-                      <img className="bulb" src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1626324777/home-page/light-bulb_1_lmqgdn.png" alt="" />
-                      <img className="book" src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1626324777/home-page/Books_kdj8sd.png" alt="" />
-                      <img className="graduation" src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1626324777/home-page/graduation_1_ueocam.png" alt="" />
-                      <div className='blob1'>
-                        
-                      </div>
-                      <div className='blob2'>
-                        
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            
-            </div>
-          </div> */}
-
           <div className="container-fluid px-10">
             <div className="row">
               <div className="col-lg-6">
                 <div class="hero-content dark-mode-texts">
-                  <h1 class="gr-text-2 mb-9 learnon">Smarter Learning for a Smarter Generation</h1>
-                  <p class="gr-text-8 pr-5 pl-0 pr-xl-11 mb-9 learnpar">When great teachers meet great technology, learning becomes easy. Enjoy trouble-free learning, and boost your preparation with Rankpedia to unlock new possibilities.  </p>
-                  {/* <div class="hero-btn">
-                    <a href="#" class="btn-link with-icon text-green1 gr-text-7 font-weight-bold">Get Started <i class="icon icon-tail-right font-weight-bold"></i></a>
-                  </div> */}
+                  <h1 class="gr-text-2 mb-9 learnon">Democratizing Global-Standard Education</h1>
+                  <p class="gr-text-8 pr-5 pl-0 pr-xl-11 mb-9 learnpar">We believe in the concept of education for all, so we strive diligently to provide equal educational opportunities to students across the world. We have built an academic ecosystem that provides an affordable world-class educational experience. </p>
                 </div>
               </div>
               <div className="col-lg-6 d-flex justify-content-center">
