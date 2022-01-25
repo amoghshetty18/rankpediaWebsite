98716be0694efae5405c488b255e74857a33abba
rjg007 <rahul@codewave.com>
2022-01-25 17:03:27 +0530

    Logo changed

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,9 +8,11 @@ const Logo = ({ className = "", ...rest }) => {
   return (
     <Link to="/" className={`${className}`} {...rest}>
       {/* <!-- light version logo (logo must be black)--> */}
-      <img src={logoW} alt="" className="light-version-logo" id="lgs-frsdt-mbds" />
+      {/* <img src={logoW} alt="" className="light-version-logo" id="lgs-frsdt-mbds" /> */}
+      <img src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1643109176/home-page/1_Rankpedia_logo_1_lgnep6.png" alt="" className="light-version-logo" id="lgs-frsdt-mbds" />
       {/* <!-- Dark version logo (logo must be White)--> */}
-      <img src={logoW} alt="" className="dark-version-logo" />
+      {/* <img src={logoW} alt="" className="dark-version-logo" /> */}
+      <img src="https://res.cloudinary.com/ddo1ag5nz/image/upload/v1643109176/home-page/1_Rankpedia_logo_1_lgnep6.png" alt="" className="dark-version-logo" />
     </Link>
   );
 };
